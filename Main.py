f60f30448b3ddf7fa823ae7a711d0aebda81dfba
protecsystems <44922400+protecsystems@users.noreply.github.com>
2025-06-21 23:45:59 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -60,5 +60,6 @@ async def handle_analysis(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("تحليل", handle_analysis))
 
-if __name__ == "__main__":
-    app.run_polling()
+    if __name__ == "__main__":
+    import asyncio
+    asyncio.run(app.run_polling())
